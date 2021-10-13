09106be9a723ddd39a48877d8cddbba2efc21803
Mario <mmdw1976@gmail.com>
2021-10-14 01:51:22 +0200

    Spliting bigger components to smaller component. Created a component wrapper Card to prevent repeating styling rules

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
 import React from 'react';
-
-import ExpenseItem from './components/ExpenseItem';
+import Card from './components/Card';
+import Expenses from './components/Expenses';
 
 const expenses = [
   {
     title: 'Car Insurance',
     amount: 297.67,
     date: new Date(2021, 2, 28),
-    id: 1
+    id: 1,
   },
   {
     title: 'OLED TV',
     amount: 999.99,
     date: new Date(2020, 3, 20),
-    id: 2
+    id: 2,
   },
   {
     title: 'Dog Food',
     amount: 14.45,
     date: new Date(2021, 11, 14),
-    id: 3
+    id: 3,
   },
   {
     title: 'Candles',
     amount: 4.56,
     date: new Date(2021, 5, 26),
-    id: 4
-  }
-]
+    id: 4,
+  },
+];
 
 const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map(expense => {
-        return (
-          <ExpenseItem key={expense.id} 
-          title={expense.title} 
-          amount={expense.amount} 
-          date={expense.date}
-          />
-        )
-      })}
+      <Card className='expenses'>
+        <Expenses expenses={expenses} />
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+
+import './Expenses.css';
+
+const Expenses = (props) => {
+    return ( 
+        <Card className="expenses">
+        {props.expenses.map(expense => {
+            return (
+              <ExpenseItem key={expense.id} 
+              title={expense.title} 
+              amount={expense.amount} 
+              date={expense.date}
+              />
+            )
+          })}
+        </Card>
+     );
+}
+ 
+export default Expenses;
