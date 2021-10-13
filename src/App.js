282c10713e214f401e359f5470c2e7f5fcf59bab
Mario <mmdw1976@gmail.com>
2021-10-13 19:36:35 +0200

    created ExpenseItem.js component and added it to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-function App() {
+import React from 'react';
+
+import ExpenseItem from './components/ExpenseItem';
+
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
